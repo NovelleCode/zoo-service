d5c9e4b31712f4cce9b2a47b67c36257b1ef81dd
Karen Batjes <batjeskaren@gmail.com>
2021-03-02 11:59:46 +0100

    Added services folder, added ZooService class,  and reformatted a bit

--- src/main/java/se/iths/zoo/Controller.java
+++ src/main/java/se/iths/zoo/Controller.java
@@ -1,7 +1,7 @@
 package se.iths.zoo;
 
 import com.iths.labbspringboot.dtos.CatDto;
-import com.iths.labbspringboot.entities.Cat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import se.iths.bird.dtos.BirdDto;
-import se.iths.bird.entities.Bird;
-import se.iths.bird.mappers.BirdMapper;
-import se.iths.bird.repositories.BirdRepository;
+
 import se.iths.zoo.entities.AnimalDto;
 import se.iths.zoo.mappers.AnimalMapper;
+import se.iths.zoo.services.ZooService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,28 +22,18 @@ public class Controller {
 
     @Autowired
     RestTemplate restTemplate;
+    private final ZooService zooService;
 
-    private AnimalMapper animalMapper;
-
-    public Controller(AnimalMapper animalMapper) {
-        this.animalMapper = animalMapper;
+    public Controller(RestTemplate restTemplate, ZooService zooService) {
+        this.restTemplate = restTemplate;
+        this.zooService = zooService;
     }
 
-
-    @GetMapping("/getzoo")
+    @GetMapping("/zoo")
     public List<AnimalDto> zoo(){
-        List<AnimalDto> animals = new ArrayList<>();
-
         var cats = this.restTemplate.getForObject("http://cats-service/cats/", CatDto[].class);
         var birds = this.restTemplate.getForObject("http://birds-service/birds/", BirdDto[].class);
-
-        for(CatDto catDto : cats) {
-            animals.add(animalMapper.mapp(catDto));
-        }
-        for(BirdDto birdDto : birds) {
-            animals.add(animalMapper.mapp(birdDto));
-        }
-        return animals;
+        return zooService.convertToAnimals(cats, birds);
     }
 
     @Bean
